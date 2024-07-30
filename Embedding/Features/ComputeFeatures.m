742074078420951be14c6444d21c83c771d109a5
Max  Weinberg <maxweinberg85@gmail.com>
2024-07-30 15:04:30 -0400

    feat: Update ComputeFeatures function to return features object
    The code changes in `ComputeFeatures.m` modify the function to return a single `features` object instead of separate `angle_features` and `distance_features` variables. This improves the organization and readability of the code.
    
    By returning a unified `features` object, developers can easily access and manipulate the computed features in a more intuitive way. This enhances the flexibility and maintainability of the codebase.

--- Embedding/Features/ComputeFeatures.m
+++ Embedding/Features/ComputeFeatures.m
@@ -1,4 +1,4 @@
-function [angle_features, distance_features] = ComputeFeatures(proc_mice_pos_data, project)
+function [features] = ComputeFeatures(proc_mice_pos_data, project)
 
 feature_list_fn_dist = project.parameters.features.distances_xlsx_Path;
 feature_table_dist = readtable(feature_list_fn_dist);
@@ -8,7 +8,7 @@ distance_features = CalculateDistanceFeatures(proc_mice_pos_data,feature_table_d
 feature_list_fn_ang = project.parameters.features.angles_xlsx_path;
 feature_table_ang = readtable(feature_list_fn_ang);
 angle_features = CalculateAngleFeatures(proc_mice_pos_data,feature_table_ang);
-angle_features = angle_features;
-distance_features = distance_features;
+features.angle_features = angle_features;
+features.distance_features = distance_features;
 
 end 
