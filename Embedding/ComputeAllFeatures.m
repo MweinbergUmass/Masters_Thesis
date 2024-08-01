fdfafd54248d87972ea790f5686e381369e91c42
Max  Weinberg <maxweinberg85@gmail.com>
2024-07-31 21:42:17 -0400

    chore: Update minmaxdata initialization in ComputeAllFeatures

--- Embedding/ComputeAllFeatures.m
+++ Embedding/ComputeAllFeatures.m
@@ -9,10 +9,10 @@ function ComputeAllFeatures(project)
     
 
     % initialize minmaxdata
-    minmaxdata.distances.min = nan(1,size(temp_features.distance_features.values,1));
-    minmaxdata.distances.max = nan(1,size(temp_features.distance_features.values,1));
-    minmaxdata.angles.min = nan(1,size(temp_features.angle_features.values,1));
-    minmaxdata.angles.max = nan(1,size(temp_features.angle_features.values,1));
+    minmaxdata.distances.min = nan(1,size(temp_features.distance_features.values,2));
+    minmaxdata.distances.max = nan(1,size(temp_features.distance_features.values,2));
+    minmaxdata.angles.min = nan(1,size(temp_features.angle_features.values,2));
+    minmaxdata.angles.max = nan(1,size(temp_features.angle_features.values,2));
 
     clear temp_proc_mice_pos_data temp_features;
 
@@ -27,12 +27,13 @@ function ComputeAllFeatures(project)
         % compute the features
         [features] = ComputeFeatures(proc_mice_pos_data, project);
         
-        minmaxdata.distances.min = nanmin([minmaxdata.distances.min; features.distance_features.values']);
-        minmaxdata.distances.max = nanmax([minmaxdata.distances.max; features.distance_features.values']);
-        minmaxdata.angles.min = nanmin([minmaxdata.angles.min; features.angle_features.values']);
-        minmaxdata.angles.max = nanmax([minmaxdata.angles.max; features.angle_features.values']);
+        minmaxdata.distances.min = nanmin([minmaxdata.distances.min; features.distance_features.values]);
+        minmaxdata.distances.max = nanmax([minmaxdata.distances.max; features.distance_features.values]);
+        minmaxdata.angles.min = nanmin([minmaxdata.angles.min; features.angle_features.values]);
+        minmaxdata.angles.max = nanmax([minmaxdata.angles.max; features.angle_features.values]);
+        
 
-        proc_mice_pos_data.features = features;
+        proc_mice_pos_data.features = features; 
 
         % save the data
         project.updateProcessingStatus(proc_mice_files{i}, 'features_extracted', 1, proc_mice_pos_data);
