6b29c945421d588b12e037cbbb4a7134a66cd1a9
Max  Weinberg <maxweinberg85@gmail.com>
2024-07-31 21:42:02 -0400

    Refactor code to ensure consistent dimensions of angle and distance features

--- Embedding/Features/ComputeFeatures.m
+++ Embedding/Features/ComputeFeatures.m
@@ -12,4 +12,11 @@ angle_features = CalculateAngleFeatures(proc_mice_pos_data,feature_table_ang);
 features.angle_features = angle_features;
 features.distance_features = distance_features;
 
+if size(features.angle_features.values,1) < size(features.angle_features.values,2)
+    features.angle_features.values = features.angle_features.values';
+end
+if size(features.distance_features.values,1) < size(features.distance_features.values,2)
+    features.distance_features.values = features.distance_features.values';
+end
+
 end 
