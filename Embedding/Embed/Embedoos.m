57c28f81d3bcc93868c021ebeffc7f533d1420b7
Max  Weinberg <maxweinberg85@gmail.com>
2024-08-01 21:48:50 -0400

    chore: Update Embedoos.m to include project status update and save

--- Embedding/Embed/Embedoos.m
+++ Embedding/Embed/Embedoos.m
@@ -15,7 +15,9 @@ function Embedoos(proc_mice_pos_data_file, project)
     % now we need to embed the data
     proc_mice_pos_data.Motmap.Zvals = project.module.embed(features_all,project.parameters.project.parameters.embedding.mlp.metadata.model_path);
     proc_mice_pos_data.Motmap.mlp_path = project.parameters.project.parameters.embedding.mlp.metadata.model_path;
-
+    % updateProcessingStatus(obj, filePath, statusType, value, processedData)
+    project.updateProcessingStatus(proc_mice_pos_data_file, 'embedding', 1, proc_mice_pos_data);
+    project.saveProject();
     
     
 end 
